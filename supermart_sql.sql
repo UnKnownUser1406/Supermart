d59ef713a5514c1ac5da302f079efd7b6f2a6dd9
Aditya14 <aditya.patel@somaiya.edu>
2026-02-01 21:47:48 +0530

    learnt and practiced sql now started with dashboard

--- supermart_sql.sql
+++ supermart_sql.sql
@@ -1,6 +1,6 @@
 USE Salesdb;
-SELECT *
-from sales ;
+/*SELECT *
+from sales ;*/
 
 /* SELECT "Order ID","Customer Name" , Sales , Profit
 from sales */
@@ -61,3 +61,88 @@ GROUP BY Region */
 /* SELECT Segment,AVG(Sales)
 FROM sales
 GROUP BY Segment */
+
+/* SELECT Region,COUNT(OrderID)
+FROM sales
+GROUP BY Region */
+
+/* SELECT SubCategory,SUM(Sales)
+FROM sales
+GROUP BY SubCategory */
+
+/* SELECT Shipmode , AVG(Profit)
+FROM sales
+GROUP BY Shipmode */
+
+/* SELECT Sales 
+FROM sales
+ORDER BY Sales Desc limit 10 */ 
+
+/* SELECT Profit
+FROM sales
+ORDER BY Profit limit 10 */
+
+/* SELECT CustomerName, SUM(Sales) AS TotalSales
+FROM sales
+GROUP BY CustomerName
+ORDER BY TotalSales desc LIMIT 5 */
+
+/* SELECT Region,sum(Profit) as TotalProfit
+FROM sales
+GROUP BY Region
+ORDER BY TotalProfit desc limit 3 */
+
+/* SELECT Category,SUM(Sales) as TotalSales
+FROM sales
+GROUP BY Category
+HAVING TotalSales > 100000 */
+
+/* SELECT CustomerName , SUM(Profit) as TotalProfit
+FROM sales 
+GROUP BY CustomerName
+HAVING TotalProfit < 0
+ORDER BY TotalProfit */
+
+/* SELECT Region,COUNT(OrderID) AS TotalOrders
+FROM sales
+GROUP BY Region
+HAVING TotalOrders > 2500 */
+
+/* SELECT s.Year,sum(Sales) as totalsales
+FROM sales as s
+GROUP BY s.Year */
+
+/* SELECT *
+FROM sales
+WHERE Sales > (SELECT AVG(Sales) FROM sales); */
+
+/* SELECT ProductName, SUM(Sales) AS Totalsales
+FROM sales
+GROUP BY ProductName
+HAVING SUM(Sales) > (SELECT AVG(Sales) FROM sales); */
+
+/* SELECT CustomerName , SUM(Sales) as HisSpending
+FROM sales
+GROUP BY CustomerName
+HAVING SUM(Sales) > (SELECT AVG(Sales) FROM sales) */
+
+
+/* WHICH CATEGORY IS MOST PROFITABLE ?
+ 
+SELECT Category,sum(Profit) as totalProfit
+from sales
+group by category
+order by totalProfit desc limit 1 */
+
+/* SELECT Region,sum(Profit) as totalprofit ,sum(Sales) as totalsales
+from sales
+group by Region
+order by totalprofit asc , totalsales desc limit 3 */
+
+/* SELECT SUM(sales) as totalsales
+from sales
+group by Products
+order by totalsales desc limit 20% */
+ 
+SELECT count(distinct OrderID)
+from sales
